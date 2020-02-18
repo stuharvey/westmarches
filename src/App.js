d02ad64642df81b7aeb23f0d32c6e4740a312108
stuharvey <stu@vt.edu>
2020-02-17 21:22:33 -0500

    Add per-tile data example; real data next

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import cx from "classnames";
+import ReactTooltip from "react-tooltip";
 
 import { Main, East, West, Hex, HexRow, Board } from "./styles";
 import { BOARD_SIZE } from "./constants";
@@ -33,22 +34,63 @@ const ROW_MAX = [
 
 const BOARD = Array(BOARD_SIZE).fill(Array(BOARD_SIZE).fill({}));
 
-function getTileColor() {
-  return PALETTE[Math.floor(Math.random() * PALETTE.length)];
-}
+const pickRandom = list => list[Math.floor(Math.random() * list.length)];
+
+const DEFAULT_HOVER = {
+  title: "Westwall",
+  description: "Home sweet home"
+};
+
+const TITLES = [
+  "Stonefoot Hills",
+  "Goblin Hill",
+  "The Mines of Moria",
+  "Boring Plains",
+  "Here Be Dragons",
+  "Seven Holy Springs",
+  "Plan for Trouble",
+  "Just a Big Hill",
+  "Death by Orc"
+];
+
+const DESCRIPTIONS = [
+  "Ten wizard towers in one zone??",
+  "You should bring a few days of rations.",
+  "[NOOBS ONLY] Definitely not filled with hydras.",
+  "Rumors abound of the mystical springs with rejuvinating properties.",
+  "Literally just grass for a whole hex.",
+  "Level 10 dungeon. Many adventurers sacrificed themselves to return this information.",
+  "Probably at least one dragon."
+];
 
 function App() {
+  const [hovered, setHovered] = useState(DEFAULT_HOVER);
+
+  const onMouseOver = useCallback(
+    () =>
+      setHovered({
+        title: pickRandom(TITLES),
+        description: pickRandom(DESCRIPTIONS)
+      }),
+    []
+  );
+  const onMouseOut = useCallback(() => setHovered(DEFAULT_HOVER), []);
+
   return (
     <Main>
       <West className="hex-container">
         <Board>
           {BOARD.map((row, i) => (
-            <HexRow className={cx({ odd: i % 2 === 1, hidden: i < 3 })}>
+            <HexRow key={i} className={cx({ odd: i % 2 === 1, hidden: i < 3 })}>
               {row.map((hex, j) => {
                 const { min, max } = ROW_MAX[i];
                 return (
                   <Hex
-                    color={getTileColor()}
+                    key={`${i}-${j}`}
+                    data-tip="React-tooltip"
+                    color={PALETTE[(i + j) % PALETTE.length]}
+                    onMouseOver={onMouseOver}
+                    onMouseOut={onMouseOut}
                     className={cx({ hidden: j + 1 <= min || j + 1 >= max })}
                   />
                 );
@@ -59,13 +101,23 @@ function App() {
             {Array(10)
               .fill({})
               .map((row, i) => (
-                <Hex color={"#383838"} className={cx({ hidden: i !== 5 })} />
+                <Hex
+                  key={`bottom-${i}`}
+                  data-tip="React-tooltip"
+                  color={"#383838"}
+                  className={cx({ hidden: i !== 5 })}
+                />
               ))}
           </HexRow>
         </Board>
       </West>
 
       <East />
+
+      <ReactTooltip type="dark">
+        <h2>{hovered.title}</h2>
+        <p>{hovered.description}</p>
+      </ReactTooltip>
     </Main>
   );
 }
