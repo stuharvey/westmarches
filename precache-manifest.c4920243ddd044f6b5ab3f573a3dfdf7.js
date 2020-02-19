f9a7a454e81950c8887ec361dcafd1537096de17
Stu Harvey <stu@vt.edu>
2020-02-18 22:52:43 -0500

    Updates

---
+++ precache-manifest.c4920243ddd044f6b5ab3f573a3dfdf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8ea145d1b9128952e94f546bde6b50",
+    "url": "/westmarches/index.html"
+  },
+  {
+    "revision": "24068eb81db03c1bdf7b",
+    "url": "/westmarches/static/css/main.6b684a32.chunk.css"
+  },
+  {
+    "revision": "cde2b2b10f98b70a538c",
+    "url": "/westmarches/static/js/2.27206a16.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/westmarches/static/js/2.27206a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24068eb81db03c1bdf7b",
+    "url": "/westmarches/static/js/main.ad29c057.chunk.js"
+  },
+  {
+    "revision": "5c15d31c05001f558136",
+    "url": "/westmarches/static/js/runtime-main.5f9ca8a6.js"
+  }
+]);
